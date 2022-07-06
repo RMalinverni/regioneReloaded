8b6c703e02a9cc64b6073a1774093fd77dd4eb20
David Corujo <dcorujog@gmail.com>
2022-07-06 15:49:43 +0200

    Working geom density with auto ramp palette

--- R/plotSingleLZ.R
+++ R/plotSingleLZ.R
@@ -20,14 +20,18 @@ plotSingleLZ <-
            RS,
            xlab = "",
            limH = NA,
-           main = NA) {
+           main = NA,
+           colpal = NULL) {
 
-    colvec<-c("#57837B","#F1ECC3","#C9D8B6","#515E63","#C05555")
-    colpal <- brewer.pal(n = 8, "Set2")
-    pal <- colorRampPalette(colpal)
+    if (is.null(colpal)) {
+      colpal <- brewer.pal(n = 5, "Set2")
+      pal <- colorRampPalette(colpal)
+    } else {
+      pal <- colorRampPalette(colpal)
+    }
 
-    mendel_theme<-theme(panel.background = element_rect(fill = colvec[2],
-                                                        colour = colvec[2],
+    mendel_theme<-theme(panel.background = element_rect(fill = "#F1ECC3",
+                                                        colour = "#F1ECC3",
                                                         size = 0.5, linetype = "solid"),
                         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                                         colour = "#FDFAF6"),
@@ -38,23 +42,18 @@ plotSingleLZ <-
       mLZ@parameters$evFUN <- "N. of overlaps"
     }
 
-
     RS <- as.list(RS)
     df<-do.call("rbind", lapply(X=RS, FUN = DFfromLZ, mLZ=mLZ))
 
-    p <- ggplot(df, aes(x = shift, y = normLocalZsore, group_by=name, fill = name, color = name)) +
-      # hline at y = 0
-      geom_hline(yintercept=0,  color =colvec[4], size=0.6) +
-      # vline at x = 0
-      geom_vline(xintercept = 0, color =colvec[4], size = 0.4, linetype = "dotted") +
-      geom_area(alpha = 0.35) +
+    p <- ggplot(df, aes(x = shift, y = normLocalZscore, group = name, fill = name, color = name)) +
+      geom_hline(yintercept=0,  color ="#515E63", size=0.6) +
+      geom_vline(xintercept = 0, color ="#515E63", size = 0.4, linetype = "dotted") +
+      geom_density(alpha = 0.2, stat = "identity") +
+      # geom_line() +
       scale_color_manual(values = pal(length(RS))) +
       scale_fill_manual(values = pal(length(RS))) +
       ylab("normalized z-score") +
       xlab("bp")
-      # labs(subtitle = paste0("Local z-score:  ", nameRS, " vs ", RS2),
-      #      caption = paste0("Original z-score :  ", zs, " - Original adj.pvalue :  ", pv)
-      # ) +
 
     if (!is.na(limH)) {
       p <- p + ylim(-limH, limH)
