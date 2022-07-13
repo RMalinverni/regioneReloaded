dde67d9f87e6175a25e0f2c780d4ec848ab06b0c
Roberto Malinverni <roberto.malinverni@gmail.com>
2022-07-13 09:23:02 +0200

    adjusting manual

--- R/genoMatriXeR_classes.R
+++ R/genoMatriXeR_classes.R
@@ -1,4 +1,12 @@
-
+#' An S4 class for "genoMatriXeR" object.
+#'
+#' @slot parameters List of parameters used to create the object
+#' @slot multiOverlaps ...
+#' @slot matrix ...
+#' @export
+#'
+#'
+#'
 gMXR <- setClass("genoMatriXeR",
                  slots = c(
                  parameters = "list",
