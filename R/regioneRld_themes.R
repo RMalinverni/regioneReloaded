9253350033a77a95999553e3a423b4d64e312453
David Corujo <dcorujog@gmail.com>
2022-07-14 16:09:11 +0200

    Specify function calls

--- R/regioneRld_themes.R
+++ R/regioneRld_themes.R
@@ -13,6 +13,8 @@
 #' @param base_size	 base font size, given in pts.
 #' @param base_family	 base font family
 #'
+#' @import ggplot2
+#'
 #'
 #' @keywords internal function
 #'
@@ -21,19 +23,19 @@
 colvec <- c("#57837B", "#F1ECC3", "#C9D8B6", "#515E63", "#C05555")
 
 mendel_theme <- function(base_size = 11, base_family = ""){
-  theme(
-    panel.background = element_rect(
+  ggplot2::theme(
+    panel.background = ggplot2::element_rect(
       fill = colvec[2],
       colour = colvec[2],
       size = 0.5,
       linetype = "solid"
     ),
-    panel.grid.major = element_line(
+    panel.grid.major = ggplot2::element_line(
       size = 0.5,
       linetype = 'solid',
       colour = "#FDFAF6"
     ),
-    panel.grid.minor = element_line(
+    panel.grid.minor = ggplot2::element_line(
       size = 0.25,
       linetype = 'solid',
       colour = "#FDFAF6"
