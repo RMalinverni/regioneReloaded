55e709263bf79238df3105c5f600914a25924d5b
David Corujo <dcorujog@gmail.com>
2022-08-04 13:48:04 +0200

    Extend getMultiEvaluation

--- R/getMultiEvaluation.R
+++ R/getMultiEvaluation.R
@@ -9,15 +9,16 @@
 #'
 #' @returns
 #' If rR is a [genoMatriXeR][genoMatriXeR-class] object, a list of data frames resuming the associations results.
-#' If rR is a [multiLocalZScore][multiLocalZScore-class] object, a list of two elements: "resumeTable" that is the resume of the associations analysis and "shifts",
-#' a list of shifts computed from [multiLocalZscore()] function for the element indicated in the nameRS vector.
+#' If rR is a [multiLocalZScore][multiLocalZScore-class] object, a list of two elements: "resumeTable" that is a data frame
+#' summarizing the associations and "shifts", a list of shifts computed from [multiLocalZscore()] function for the elements
+#' indicated in the nameRS vector.
 #'
 #' @seealso [genoMatriXeR][genoMatriXeR-class], [multiLocalZScore][multiLocalZScore-class]
 #'
 #' @export getMultiEvaluation
 #'
 
-getMultiEvaluation <- function(rR,namesRS = NA){
+getMultiEvaluation <- function(rR,namesRS = NULL){
 
   if (!methods::hasArg(rR)) {
     stop("rR is missing")
@@ -26,19 +27,30 @@ getMultiEvaluation <- function(rR,namesRS = NA){
 
   if(methods::is(rR , "genoMatriXeR" )){
 
-    if (is.na(namesRS)) { namesRS <- names(rR@multiOverlaps) }
-
+    if (!is.null(namesRS)) {
+      if (!all(namesRS %in% names(rR@multiOverlaps))) {
+        warning("One or more of namesRS is not a name of a region set in rR")
+      }
+    } else {
+      namesRS <- names(rR@multiOverlaps)
+    }
     res <- rR@multiOverlaps[namesRS]
+    res <- res[!sapply(res, is.null)]
   }
 
   if(methods::is(rR , "multiLocalZScore" )){
 
-    if (is.na(namesRS)) {
-      namesRS <- names(rR@multiLocalZscores$shifed_ZSs) }
-
-    res <- list(resumeTable = rR@multiLocalZscores$resumeTab ,
+    if (!is.null(namesRS)) {
+      if (!all(namesRS %in% names(rR@multiLocalZscores$shifed_ZSs))) {
+        warning("One or more of namesRS is not a name of a region set in rR")
+      }
+    } else {
+      namesRS <- names(rR@multiLocalZscores$shifed_ZSs)
+    }
+    res <- list(resumeTable = rR@multiLocalZscores$resumeTab,
                 shifts = rR@multiLocalZscores$shifed_ZSs[namesRS])
-  }
+    res[["shifts"]] <- res[["shifts"]][!sapply(res[["shifts"]], is.null)]
+    }
 
   return(res)
 }
