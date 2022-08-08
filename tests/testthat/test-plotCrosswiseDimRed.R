6aa50805ac3a96329a70739301a96f0abda0b799
David Corujo <dcorujog@gmail.com>
2022-08-08 16:27:22 +0200

    Testing

--- tests/testthat/test-plotCrosswiseDimRed.R
+++ tests/testthat/test-plotCrosswiseDimRed.R
@@ -1,4 +1,11 @@
 test_that("Argument checks", {
   expect_error(plotCrosswiseDimRed(), "missing")
-  expect_error(plotCrosswiseDimRed("a"))
+  expect_error(plotCrosswiseDimRed(cw_Alien_ReG), "empty")
+  expect_error(plotCrosswiseDimRed("a"), "genoMatriXeR")
+})
+
+test_that("Returns the correct class", {
+  cw_Alien_ReG <- makeCrosswiseMatrix(cw_Alien_ReG)
+  expect_s3_class(plotCrosswiseDimRed(cw_Alien_ReG), "ggplot")
+  expect_s3_class(plotCrosswiseDimRed(cw_Alien_ReG, return_table = TRUE), "data.frame")
 })
