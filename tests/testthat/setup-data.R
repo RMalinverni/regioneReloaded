7590431e37053fd759e49e991860ba4b2837524b
David Corujo <dcorujog@gmail.com>
2022-08-10 11:29:39 +0200

    Reduce small region set lists for testing

--- tests/testthat/setup-data.R
+++ tests/testthat/setup-data.R
@@ -5,8 +5,8 @@ data("cw_Alien")
 # Define small subsets of the region set lists to test crosswisePermTest() with
 # a small computation time
 
-sel <- c("regA", "regA_08", "regB", "regB_08", "regAB")
-sel_br <- c("regA_nbr", "regA_br_08", "regB_br", "regB_br_08", "regAB_br_08")
+sel <- c("regA", "regB", "regAB")
+sel_br <- c("regA_br", "regB_br", "regAB_br")
 
 AlienRSList_narrow_small <- AlienRSList_narrow[sel]
 AlienRSList_broad_small <- AlienRSList_broad[sel]
