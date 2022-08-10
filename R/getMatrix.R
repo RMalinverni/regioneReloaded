c18843af8f68e95560c5984bce4e5dd65be61d25
David Corujo <dcorujog@gmail.com>
2022-08-10 12:02:55 +0200

    Replace error by warning when getting a NULL matrix

--- R/getMatrix.R
+++ R/getMatrix.R
@@ -50,14 +50,14 @@ getMatrix <- function(rR) {
   if (methods::is(rR, "genoMatriXeR")) {
 
     if (is.null(rR@matrix[[1]])){
-      stop ("no matrix computed for genoMatriXeR object")
+      warning("no matrix computed for genoMatriXeR object, run first makeCrosswiseMatrix()")
     }
     GM <- rR@matrix$GMat
   }
 
   if (methods::is(rR, "multiLocalZScore")) {
     if (is.null(rR@matrix[[1]])){
-      stop ("no matrix computed for multiLocalZScore object")
+      warning("no matrix computed for multiLocalZScore object, run first makeLZMatrix")
     }
 
     GM <- rR@matrix$LZM
