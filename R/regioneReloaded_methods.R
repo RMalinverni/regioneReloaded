0a2afb8f51a4f664fbd131825a24395300a8403c
RMalinverni <roberto.malinverni@gmail.com>
2022-08-17 10:38:13 +0200

    adding methods for import slot for classes genoMatriXeR and multiLocalZScore

--- R/regioneReloaded_methods.R
+++ R/regioneReloaded_methods.R
@@ -19,6 +19,97 @@ setMethod("print", "genoMatriXeR", function(x)
     matrix_slot = x@matrix$GMat
   ))
 
+#' Export parameters from genoMatriXeR objects
+#'
+#' @param x A genoMatriXeR object.
+#'
+#' @return A printed output for genoMatriXeR parameters slot.
+#'
+#' @seealso [genoMatriXeR][genoMatriXeR-class]
+#'
+#' @export "gmxrParam"
+#'
+setGeneric("gmxrParam", function(x) standardGeneric("gmxrParam"))
+
+setMethod("gmxrParam", "genoMatriXeR", function(x) x@parameters)
+
+
+#' Export multiOverlaps slot from genoMatriXeR objects
+#'
+#' @param x A genoMatriXeR object.
+#'
+#' @return A printed output for genoMatriXeR multiOverlaps slot.
+#'
+#' @seealso [genoMatriXeR][genoMatriXeR-class]
+#'
+#' @export "gmxrMultiOverlaps"
+#'
+setGeneric("gmxrMultiOverlaps", function(x) standardGeneric("gmxrMultiOverlaps"))
+
+setMethod("gmxrMultiOverlaps", "genoMatriXeR", function(x) x@multiOverlaps)
+
+
+#' Export matrix slot from genoMatriXeR objects
+#'
+#' @param x A genoMatriXeR object.
+#'
+#' @return A printed output for genoMatriXeR matrix slot.
+#'
+#' @seealso [genoMatriXeR][genoMatriXeR-class]
+#'
+#' @export "gmxrMatrix"
+#'
+#'
+setGeneric("gmxrMatrix", function(x) standardGeneric("gmxrMatrix"))
+
+setMethod("gmxrMatrix", "genoMatriXeR", function(x) x@matrix)
+
+
+#' Export parameters from multiLocalZScore objects
+#'
+#' @param x A multiLocalZScore object.
+#'
+#' @return A printed output for multiLocalZScore parameters slot.
+#'
+#' @seealso [multiLocalZScore][multiLocalZScore-class]
+#'
+#' @export "mlzsParam"
+#'
+setGeneric("mlzsParam", function(x) standardGeneric("mlzsParam"))
+
+setMethod("mlzsParam", "multiLocalZScore", function(x) x@parameters)
+
+
+#' Export multiLocalZscores slot from multiLocalZScore objects
+#'
+#' @param x A multiLocalZScore object.
+#'
+#' @return A printed output for multiLocalZScore multiLocalZscores slot.
+#'
+#' @seealso [multiLocalZScore][multiLocalZScore-class]
+#'
+#' @export "mlzsMultiLocalZscores"
+#'
+setGeneric("mlzsMultiLocalZscores", function(x) standardGeneric("mlzsMultiLocalZscores"))
+
+setMethod("mlzsMultiLocalZscores", "multiLocalZScore", function(x) x@multiLocalZscores)
+
+
+
+
+#' Export matrix from multiLocalZScore objects
+#'
+#' @param x A multiLocalZScore object.
+#'
+#' @return A printed output for multiLocalZScore matrix slot.
+#'
+#' @seealso [multiLocalZScore][multiLocalZScore-class]
+#'
+#' @export "mlzsMatrix"
+#'
+setGeneric("mlzsMatrix", function(x) standardGeneric("mlzsMatrix"))
+
+setMethod("mlzsMatrix", "multiLocalZScore", function(x) x@matrix)
 
 
 
