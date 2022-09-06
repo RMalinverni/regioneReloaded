e65217cc7fa9793b108244af4a5ec91993b1f365
Roberto Malinverni <roberto.malinverni@gmail.com>
2022-09-06 09:35:36 +0200

    ADD example to classes-Rd

--- R/regioneReloaded_classes.R
+++ R/regioneReloaded_classes.R
@@ -7,6 +7,20 @@
 #' @slot parameters List of parameters used to create the object.
 #' @slot multiOverlaps Results of multiple pairwise permutation tests generated with [crosswisePermTest()].
 #' @slot matrix List of numerical matrices containing z-score, pvalues and correlation values generated with [makeCrosswiseMatrix()]
+#'
+#' @examples
+#'
+#' data("cw_Alien")
+#'
+#' AlienRSList_narrow_small  <- AlienRSList_narrow[c("regA","regB","regC")]
+#'
+#' cw_test <- crosswisePermTest(Alist = AlienRSList_narrow_small,Blist = AlienRSList_narrow_small,
+#'                             sampling = FALSE, genome = AlienGenome, per.chromosome = TRUE,
+#'                             ranFUN = "resampleGenome", evFUN = "numOverlaps",
+#'                             ntimes = 10, mc.cores = 2)
+#'
+#' class(cw_test)
+#'
 #' @export
 #'
 
@@ -27,6 +41,20 @@ gMXR <- setClass("genoMatriXeR",
 #' @slot parameters List of parameters used to create the object
 #' @slot multiLocalZscores Results of multiple pairwise permutation tests on shifted region sets generated with [multiLocalZscore()].
 #' @slot matrix List of numerical matrices containing local z-scores and correlation values generated with [makeLZMatrix()].
+#'
+#' @examples
+#'
+#' data("cw_Alien")
+#'
+#' AlienRSList_narrow_small  <- AlienRSList_narrow[c("regA","regB","regC")]
+#'
+#' mlz_test <- multiLocalZscore(A = AlienRSList_narrow_small$regA, Blist = AlienRSList_narrow_small,
+#'                             sampling = FALSE, genome = AlienGenome, per.chromosome = TRUE,
+#'                             ranFUN = "resampleGenome", evFUN = "numOverlaps",
+#'                             ntimes = 10, mc.cores = 2)
+#'
+#' class(mlz_test)
+#'
 #' @export
 #'
 
